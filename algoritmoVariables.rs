214c9fcf7537f1f0c5c30a271349db39d6859439
Darloscode <carfgonz@espol.edu.ec>
2025-11-03 23:19:46 -0500

    Se agregó nuevas reglas y tokens

---
+++ algoritmoVariables.rs
@@ -0,0 +1,6 @@
+let edad: i32 = 25;
+mut nombre: String = "Carlos";
+const PI: f64 = 3.14159;
+let activo: bool = true;
+let mut tupla = (10, 3.14, true, 'A');
+let letra: char = 'A';
--- rustAnalizer.py
+++ rustAnalizer.py
@@ -1,7 +1,9 @@
 from ply import lex
+import datetime
+import os
 
 reserved = {
-    #AVANCE DE PALABRAS RESERVADAS: Nicolas Sierra - INICIO
+    # AVANCE DE PALABRAS RESERVADAS: Nicolas Sierra - INICIO
     'as': 'AS',
     'break': 'BREAK',
     'const': 'CONST',
@@ -19,11 +21,12 @@ reserved = {
     'then': 'THEN',
     'main': 'MAIN',
     'println': 'PRINTLN',
-    #AVANCE DE PALABRAS RESERVADAS: Nicolas Sierra - FIN
+    'fn':'FN',
+    # AVANCE DE PALABRAS RESERVADAS: Nicolas Sierra - FIN
 }
 
 tokens = (
-    #AVANCE DE OPERADORES: Nicolás Sierra - INICIO
+    # AVANCE DE OPERADORES: Nicolás Sierra - INICIO
 
     # Aritméticos
     'PLUS', 'MINUS', 'TIMES', 'DIVIDE', 'MOD',
@@ -41,60 +44,71 @@ tokens = (
     # Bit a bit
     'BIT_AND', 'BIT_OR', 'BIT_XOR', 'BIT_NOT', 'SHIFT_LEFT', 'SHIFT_RIGHT',
 
-    
-
+    # AVANCE DE TOKENS PARA VARIABLES: Carlos Flores - INICIO
+    'IDENTIFICADOR',
+    'TYPE',    
+    'SEMICOLON',
+    'COLON',
+    'STRING',
+    'COMMA',
+    'CHAR',
+    'LPAREN',
+    'RPAREN',
+    'BOOLEAN',
+    'INTEGER',
+    'FLOAT',
+    # AVANCE DE TOKENS PARA VARIABLES: Carlos Flores - FIN
 )+tuple(reserved.values())
 
-
-
 # OPERADORES ARITMÉTICOS
-
-t_PLUS   = r'\+'
-t_MINUS  = r'-'
-t_TIMES  = r'\*'
+t_PLUS = r'\+'
+t_MINUS = r'-'
+t_TIMES = r'\*'
 t_DIVIDE = r'/'
-t_MOD    = r'%'
+t_MOD = r'%'
 
 # OPERADORES DE COMPARACIÓN
-
-t_EQUAL_TO    = r'=='
-t_NOT_EQUAL       = r'!='
-t_LESS_THAN_OR_EQUAL_TO      = r'<='
-t_GREATER_THAN_OR_EQUAL_TO   = r'>='
-t_LESS_THAN       = r'<'
-t_GREATER_THAN    = r'>'
-
+t_EQUAL_TO = r'=='
+t_NOT_EQUAL = r'!='
+t_LESS_THAN_OR_EQUAL_TO = r'<='
+t_GREATER_THAN_OR_EQUAL_TO = r'>='
+t_LESS_THAN = r'<'
+t_GREATER_THAN = r'>'
 
 # OPERADORES LÓGICOS
-
 t_CONJUNCTION = r'&&'
-t_DISJUNCTION   = r'\|\|'
-t_NOT     = r'!'
-
+t_DISJUNCTION = r'\|\|'
+t_NOT = r'!'
 
 # OPERADORES DE ASIGNACIÓN
-
-t_ASIGNED_TO    = r'='
-t_PLUS_EQUAL    = r'\+='
-t_MINUS_EQUAL   = r'-='
-t_TIMES_EQUAL   = r'\*='
-t_DIVIDE_EQUAL  = r'/='
-t_MOD_EQUAL     = r'%='
-
+t_ASIGNED_TO = r'='
+t_PLUS_EQUAL = r'\+='
+t_MINUS_EQUAL = r'-='
+t_TIMES_EQUAL = r'\*='
+t_DIVIDE_EQUAL = r'/='
+t_MOD_EQUAL = r'%='
 
 # OPERADORES BIT A BIT
-
-t_BIT_AND     = r'&'
-t_BIT_OR      = r'\|'
-t_BIT_XOR     = r'\^'
-t_BIT_NOT     = r'~'
-t_SHIFT_LEFT  = r'<<'
+t_BIT_AND = r'&'
+t_BIT_OR = r'\|'
+t_BIT_XOR = r'\^'
+t_BIT_NOT = r'~'
+t_SHIFT_LEFT = r'<<'
 t_SHIFT_RIGHT = r'>>'
+
+# AVANCE SIGNOS: Carlos Flores - INICIO
+# SIGNOS
+t_SEMICOLON = r'\;'
+t_COLON = r'\:'
+t_COMMA = r','
+t_LPAREN  = r'\('
+t_RPAREN  = r'\)'
+# AVANCE SIGNOS: Carlos Flores - FIN
 
 # Ignorar espacios y tabulaciones
 t_ignore = ' \t'
 
-#Define los booleanos true y false
+# Define los booleanos true y false
 def t_BOOLEAN(t):
     r'true|false'
     return t
@@ -104,10 +118,80 @@ def t_error(t):
     print("Componente lexico '%s' no existe en el lenguaje Rust" % t.value[0])
     t.lexer.skip(1)
 
-#AVANCE DE OPERADORES: Nicolás Sierra - FIN
+# AVANCE DE OPERADORES: Nicolás Sierra - FIN
 
-#Definición de variables
-def t_VARIABLE(t):
-    r'[a-z]\w*'
-    t.type = reserved.get(t.value, "VARIABLE") #Verifico que no haga match con alguna de las funciones reservadas
+# AVANCE PARA DEFINIR VARIABLES: Carlos Flores - INICIO
+# Regla para cadenas de texto (entre comillas)
+def t_CHAR(t):
+    r"'(\\.|[^\\'])'"
+    t.value = t.value[1:-1]  # elimina las comillas simples
     return t
+
+def t_STRING(t):
+    r'\"([^\\\n]|(\\.))*?\"'
+    t.value = t.value.strip('"')
+    return t
+
+# Reglas de tipo de datos
+def t_TYPE(t):
+    r'i32|u64|f64|char|String|bool|tupla'
+    return t
+
+# Regla para identificar variables en Rust
+def t_IDENTIFICADOR(t):
+    r'[a-zA-Z_][a-zA-Z0-9_]*'
+    t.type = reserved.get(t.value, 'IDENTIFICADOR')
+    return t
+
+# Regla para identificar números decimales
+def t_FLOAT(t):
+    r'\d+\.\d+'
+    t.value = float(t.value)
+    return t
+
+# Regla para identificar números enteros
+def t_INTEGER(t):
+    r'\d+'
+    t.value = int(t.value)
+    return t
+
+# Regla para identificar saltos de línea
+def t_newline(t):
+    r'\n+'
+    t.lexer.lineno += len(t.value)
+# AVANCE PARA DEFINIR VARIABLES: Carlos Flores - FIN
+
+
+# Give the lexer some input
+archivos = {"Carlos Flores":["algoritmoVariables.rs"], 
+            "Nicolas Sierra":["algoritmoOperadores.rs"]}
+
+# Build the lexer
+lexer = lex.lex()
+
+ahora = datetime.datetime.now()
+fecha = ahora.strftime("%d-%m-%Y")
+hora = ahora.strftime("%Hh%M")
+
+for name,archivos in archivos.items():
+
+    nombre_log = f"./logs/{name.replace(" ","_")}/lexico-{name.replace(" ","")}-{fecha}-{hora}.txt"
+    log = open(nombre_log, "w", encoding="utf-8")
+
+    for archivo in archivos:
+        if not os.path.exists(archivo):
+            print(f"ALERTA: El archivo '{archivo}' no existe.")
+        else:
+                with open(archivo, "r", encoding="utf-8") as file:
+                    for num_linea, linea in enumerate(file, start=1):
+                        print(f"\nLínea {num_linea}: {linea.strip()}")
+                        log.write(f"\nLínea {num_linea}: {linea.strip()}\n")
+                        lexer.input(linea)
+                        while True:
+                            tok = lexer.token()
+                            if not tok:
+                                break
+                            linea_log = f"[TOKEN] Tipo: {tok.type:<15} | Valor: {tok.value:<15} | Línea: {num_linea:<3} | Posición: {tok.lexpos}\n"
+                            print(linea_log.strip())
+                            log.write(linea_log)
+log.close()
